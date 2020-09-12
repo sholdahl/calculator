8fc35af7393b6e208c009bb8d2da7bc4f37daf8a
sholdahl <sam.holdahl@gmail.com>
2020-09-11 22:02:52 -0500

    created basic math functions and operation function. added links to bootstrap

---
+++ main.js
@@ -0,0 +1,26 @@
+const add = (num1, num2) => {
+    return num1 + num2;
+}
+const subtract = (num1, num2) => {
+    return num1 - num2;
+}
+const multiply = (num1, num2) => {
+    return num1 * num2;
+}
+const divide = (num1, num2) => {
+    return num1 / num2;
+}
+
+function operate(num1, num2, operation) {
+    if(operation = "+") {
+        add(num1, num2)
+    } else if(operation = "-"){
+        subtract(num1, num2)
+    } else if(operation = "*"){
+        multiply(num1, num2)
+    } else if(operation = "/"){
+        divide(num1, num2)
+    } else {
+        return "ERROR"
+    }
+}
