5d2d210ea114dae425c2de8a2c4fbdc057394ed0
sholdahl <sam.holdahl@gmail.com>
2020-10-03 14:37:50 -0500

    decimal fixed. Calculator now complete.

--- main.js
+++ main.js
@@ -2,12 +2,11 @@
 let displayText = document.querySelector(".display-text");
 let buttons = document.querySelectorAll(".button");
 let operator = "";
-let displayValue = 0;
-let storedValue = 0;
-let decimalToggle = "off"
+let displayValue = "0";
+let storedValue = "0";
 
 // Event Listeners
- window.addEventListener("keydown", handleKeypress);
+window.addEventListener("keydown", handleKeypress);
 buttons.forEach(button => {
     button.addEventListener("click", handleClick)
 })
@@ -24,45 +23,41 @@ function handleClick() {
 };
 
 function handleInput(input) {
-    console.log(decimalToggle);
-    if([0,1,2,3,4,5,6,7,8,9].indexOf(Number(input)) !== -1) {
-        if(decimalToggle == "off") {
-            displayValue = displayValue * 10 + Number(input);
-        } else {
-            console.log(displayValue);
-            console.log(decimalToggle);
-            displayValue.concat(input);
-        }
-    } else if(input == "c") {
-        displayValue = 0;
-        storedValue = 0;
+    if ([0, 1, 2, 3, 4, 5, 6, 7, 8, 9].indexOf(Number(input)) !== -1) {
+        displayValue = displayValue.concat(input);
+    } else if (input == "c") {
+        displayValue = "0";
+        storedValue = "0";
         operator = "";
-    } else if(input == "ce") {
-        displayValue = 0;
-    } else if(["+","-","/","*","÷","x"].indexOf(input) !== -1) {
+    } else if (input == "ce") {
+        displayValue = "0";
+    } else if (["+", "-", "/", "*", "÷", "x"].indexOf(input) !== -1) {
         storedValue = displayValue;
-        displayValue = 0;
+        displayValue = "0";
         operator = input;
-        decimalToggle = "off"
-    } else if(input == "=" || input == "enter") {
-        if(operator !== "") {
-            displayValue = operate(storedValue, displayValue, operator);
+    } else if (input == "=" || input == "enter") {
+        if (operator !== "") {
+            displayValue = operate(Number(storedValue), Number(displayValue), operator).toString();
             operator = "";
             storedValue = "";
         }
-    } else if(input == "+/-" || input == "s") {
-        displayValue = displayValue * -1;
-    } else if(input == "⌫" || input == "backspace"){
-        if(displayValue == 0){
+    } else if (input == "+/-" || input == "s") {
+        displayValue = (Number(displayValue) * -1).toString();
+    } else if (input == "⌫" || input == "backspace") {
+        if (displayValue == "0") {
             return
         } else {
-            let numLength = displayValue.toString().length;
-            displayValue = Number(displayValue.toString().substring(0,numLength-1));
+            let numLength = displayValue.length;
+            displayValue = displayValue.substring(0, numLength - 1);
         }
-    } else if(input == "." && decimalToggle == "off") {
-        displayValue = displayValue.toString().concat(".");
-        decimalToggle = "on"
-    } 
+    } else if (input == "." && !displayValue.includes(".")) {
+            displayValue = displayValue.concat(".");
+    }
+    if(displayValue == ""){
+        displayValue = "0"
+    } else if(displayValue.length > 1 && displayValue[0] == "0"){
+        displayValue = displayValue.substring(1, displayValue.length)
+    }
     displayText.textContent = displayValue;
 }
 
@@ -81,13 +76,13 @@ const divide = (num1, num2) => {
 }
 
 function operate(num1, num2, operation) {
-    if(operation == "+") {
+    if (operation == "+") {
         return add(num1, num2);
-    } else if(operation == "-"){
+    } else if (operation == "-") {
         return subtract(num1, num2);
-    } else if(operation == "*" || operation == "x"){
+    } else if (operation == "*" || operation == "x") {
         return multiply(num1, num2);
-    } else if(operation == "/" || operation == "÷"){
+    } else if (operation == "/" || operation == "÷") {
         return divide(num1, num2);
     } else {
         return "ERROR"
